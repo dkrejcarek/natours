16fed9d3d0cd621a76fba288a1295e1f9c9e2db1
David Krejcarek <dkrejcarek82@gmail.com>
2020-08-16 12:17:31 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(__dirname))
+
+app.listen(PORT, (req, res) => {
+	console.log(`Natours is running ${PORT}`)
+})
